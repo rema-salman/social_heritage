e26f08ee91484a8bd17a683fec50fadd0f1894fc
rema-salman <rema.salman@outlook.com>
2021-03-17 19:19:50 +0100

    final changes

--- web_client/src/main.js
+++ web_client/src/main.js
@@ -1,9 +1,3 @@
-var colors = ["red", "blue"];
-var imgNum = "number of images ";
-var langC = [25, 51.505, 43.37667, 56.87767, 57.70887];
-var longC = [0, -0.09, 24.61667, 14.80906, 11.97456];
-var zoomL = 13;
-
 // PURE LEAFLET MAP -----------------------------------
 var leaflet_map = L.map("mapid").setView([0, 0], 3);
 var access_token =
@@ -34,8 +28,6 @@ getSelectedOptionValue();
 //gets the value of choosesn location from the menu with options
 function getSelectedOptionValue() {
   var selectedOption = document.getElementById("optionList").value;
-  console.log(selectedOption);
-
   if (selectedOption == 1) {
     leaflet_map.flyTo(gothenburg, 13);
     getWetherConditions("gothenburg");
@@ -76,15 +68,13 @@ const displayWeatherCardInfo = (weatherConditions) => {
   cardContent.innerHTML = `pressure ${weatherConditions.main.pressure} and humidity ${weatherConditions.main.humidity}`;
 };
 
-// Load Data and display them -------------------------------------
-//sets the view of the map, markers and popups
+// Fetch Data and display on map -------------------------------------
 getPosts();
 function getPosts() {
   axios
     .get("http://localhost:5000/posts")
     .then(function (response) {
       // handle success
-      console.log(response);
       handelDataDisplay(response.data);
     })
     .catch(function (error) {
@@ -96,20 +86,47 @@ function getPosts() {
     });
 }
 
-function handelDataDisplay(data) {
-  console.log(data);
+function getLocationString(postLocation) {
+  return postLocation.latitude.toString() + postLocation.longitude.toString();
+}
+
+function unifyLocations(data) {
+  const checkedLocations = [];
+  const unifiedLocations = [];
   //markers and mopups
   for (var i = 0; i < data.length; i++) {
-    color = "red";
-    num = 5;
-    if (num < 5) {
-      color = "red;";
-    } else if ((num) => 5) {
+    const locationPair = getLocationString(data[i].postLocation);
+
+    if (checkedLocations.includes(locationPair)) {
+      //already checked
+    } else {
+      checkedLocations.push(locationPair);
+      const currectPostLocation = getLocationString(data[i].postLocation);
+      const samePositionPlaces = data.filter(
+        (e) => getLocationString(e.postLocation) === currectPostLocation
+      );
+      //array of arrays
+      if (samePositionPlaces.length) unifiedLocations.push(samePositionPlaces);
+    }
+  }
+  return unifiedLocations;
+}
+//sets the view of the map, markers and popups
+function handelDataDisplay(data) {
+  const unifiedLocations = unifyLocations(data);
+  for (var i = 0; i < unifiedLocations.length; i++) {
+    if (unifiedLocations[i].length > 5) {
+      color = "red";
+    } else {
       color = "blue";
     }
+
     //draws a circle at location
     var circle = L.circle(
-      [data[i].postLocation.latitude, data[i].postLocation.longitude],
+      [
+        unifiedLocations[i][0].postLocation.latitude,
+        unifiedLocations[i][0].postLocation.longitude,
+      ],
       {
         color: color,
         fillColor: color,
@@ -117,26 +134,92 @@ function handelDataDisplay(data) {
         radius: 150,
       }
     ).addTo(leaflet_map);
+    for (var j = 0; j < unifiedLocations[i].length; j++) {
+      //adds popup to this circle
 
-    //adds popup to this circle
-    circle.bindPopup(
-      "<h1>Hestagram</h1>" +
-        `<img src=${data[0].postPhoto} alt='images' class='herImg' /> <br>` +
-        data[i].caption +
+      const circleContentDiv = document.createElement("div");
+      const circleContent = document.createElement("div");
+      circleContent.innerHTML =
+        "<h1>Inherito</h1>" +
+        `<img src=${unifiedLocations[i][j].postPhoto} alt='images' class='herImg' /> <br>` +
+        unifiedLocations[i][j].caption +
         "<br>" +
-        data[i].date +
+        unifiedLocations[i][j].date +
         "<br>" +
-        data.length +
-        "<br>" +
-        "<a href = 'gallery.html'>See more</a>"
-    );
+        unifiedLocations[i].length +
+        "<br>";
+      circleContentDiv.appendChild(circleContent);
 
-    // var img = document.getElementById("img").src;
-    // img = data[0].postPhoto;
+      const seeMoreButon = document.createElement("button");
+      // seeMoreButon.href = "#";
+      seeMoreButon.id = `action${i}${j}`;
+      seeMoreButon.innerHTML = "See more";
+      seeMoreButon.addEventListener("click", function (e) {
+        // e.preventDefault();
+        alert(1);
+        catchImg(unifiedLocations[i]);
+      });
+
+      circleContentDiv.appendChild(seeMoreButon);
+
+      circle.bindPopup(circleContentDiv.innerHTML);
+    }
 
     //the pop up displays on hover intead of onClick as it is by default
     circle.on("mouseover", function (event) {
       event.target.openPopup();
     });
   }
+}
+
+// Gallery ////////////--------------
+//to open a gallery and display images in it
+function catchImg(data) {
+  var galdiv = document.getElementById("galrow");
+  galdiv.style.display = "block";
+  var close = document.createElement("BUTTON");
+  close.innerHTML = "X";
+  close.classList.add("close-btn");
+  galdiv.appendChild(close);
+  close.addEventListener("click", function () {
+    galdiv.style.display = "none";
+  });
+  const img = document.createElement("img");
+
+  data.forEach((element) => {
+    console.log("from gallery" + element);
+    let imgItem = element.postPhoto;
+
+    img.src = imgItem; //////////// image
+    img.alt = "image";
+    img.style.width = "100%";
+  });
+
+  const div1 = document.createElement("div");
+  div1.classList.add("col-md-3");
+  div1.style.marginTop = "15px";
+  galdiv.appendChild(div1);
+
+  const div2 = document.createElement("div");
+  div2.classList.add("tumbnail");
+  div1.appendChild(div2);
+
+  const caption = document.createElement("p");
+  caption.classList.add("caption");
+  const nodeText = document.createTextNode("Caption from data");
+  caption.appendChild(nodeText); //apppend the text to the paragraph
+  div2.appendChild(caption); //append the caption to the subdiv
+
+  const a1 = document.createElement("a");
+  a1.href = img.src;
+  a1.target = "_blank";
+  div2.appendChild(a1);
+
+  a1.appendChild(img);
+  const capt = document.createElement("p");
+  capt.classList.add("caption");
+  const node = document.createTextNode("Click on the images to enlarge them");
+  capt.appendChild(node);
+  a1.appendChild(capt);
+  // Append the img to the link
 }

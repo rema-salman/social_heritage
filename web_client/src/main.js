95c0702c61748cf7a39a443a2fc8f27ba28f443c
rema-salman <rema.salman@outlook.com>
2021-03-17 02:49:02 +0100

    web client

--- web_client/src/main.js
+++ web_client/src/main.js
@@ -1,69 +1,103 @@
-var data = [
-  {
-    postLocation: {
-      latitude: 57.70887,
-      longitude: 11.97456,
-    },
-    caption: "Image from a poster",
-    postPhoto: "hesta2.png", //url source
-    date: "new Data()", //newDate
-  },
-  {
-    postLocation: {
-      latitude: 57.7,
-      longitude: 11.97456,
-    },
-    caption: "Drawing by memory",
-    postPhoto: "hesta.png", //url source
-    date: "new Data()", //newDate
-  },
-  {
-    postLocation: {
-      latitude: 57.71767,
-      longitude: 11.97,
-    },
-    caption: "Another drawing by memory 2",
-    postPhoto: "hesta.png", //url source
-    date: "new Data()", //newDate
-  },
-];
-
 var colors = ["red", "blue"];
 var imgNum = "number of images ";
 var langC = [25, 51.505, 43.37667, 56.87767, 57.70887];
 var longC = [0, -0.09, 24.61667, 14.80906, 11.97456];
 var zoomL = 13;
-//sets the view at the center of the world mad
-loadMap(langC[4], longC[4], zoomL);
 
-//sets the view of the map, markers and popups
-function loadMap(lat, lon, zoom) {
-  var mymap = L.map("mapid").setView([lat, lon], zoom);
-  var access_token =
-    "pk.eyJ1IjoianVsaWp1IiwiYSI6ImNrbTgyMHFlZDEzZ3IyeW42OTg1N2VjZ3EifQ.kxwuRnukhgVKGZUPQGIePw";
+// PURE LEAFLET MAP -----------------------------------
+var leaflet_map = L.map("mapid").setView([0, 0], 3);
+var access_token =
+  "pk.eyJ1IjoianVsaWp1IiwiYSI6ImNrbTgyMHFlZDEzZ3IyeW42OTg1N2VjZ3EifQ.kxwuRnukhgVKGZUPQGIePw";
 
-  //map centering and zoom
-  L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      minZoom: 2,
-      id: "mapbox/streets-v11",
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: access_token,
-    }
-  ).addTo(mymap);
-  displayData();
+//map tiles, centering and zoom
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 2,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: access_token,
+  }
+).addTo(leaflet_map);
+
+// Cities: Zoom Centers -------------------------------------------
+var gothenburg = [57.70887, 11.97456];
+var london = [51.505, 0];
+var Kaylaka_park = [43.41667, 24.61667];
+var vaxjo = [56.879, 14.8059];
+
+getSelectedOptionValue();
+//gets the value of choosesn location from the menu with options
+function getSelectedOptionValue() {
+  var selectedOption = document.getElementById("optionList").value;
+  console.log(selectedOption);
+
+  if (selectedOption == 1) {
+    leaflet_map.flyTo(gothenburg, 13);
+    getWetherConditions("gothenburg");
+  } else if (selectedOption == 2) {
+    leaflet_map.flyTo(london, 13);
+    getWetherConditions("london");
+  } else if (selectedOption === 3) {
+    leaflet_map.flyTo(Kaylaka_park, 13);
+    getWetherConditions("kaylaka");
+  } else if (selectedOption === 4) {
+    leaflet_map.flyTo(vaxjo, 13);
+    getWetherConditions("vaxjo");
+  }
 }
 
-function displayData() {
-  fetch("http://localhost:5000/posts")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+// Fetch weather condition based on city -------------------------------------------
+const getWetherConditions = (city) => {
+  axios
+    .get("http://localhost:5000/weather", {
+      params: { city: city },
+    })
+    .then(function (response) {
+      // handle success
+      let weatherConditions = JSON.parse(response.data.body);
+      displayWeatherCardInfo(weatherConditions);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+};
 
+const displayWeatherCardInfo = (weatherConditions) => {
+  const cardContent = document.getElementById("card-title");
+  cardContent.innerHTML = `pressure ${weatherConditions.main.pressure} and humidity ${weatherConditions.main.humidity}`;
+};
+
+// Load Data and display them -------------------------------------
+//sets the view of the map, markers and popups
+getPosts();
+function getPosts() {
+  axios
+    .get("http://localhost:5000/posts")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      handelDataDisplay(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+}
+
+function handelDataDisplay(data) {
+  console.log(data);
   //markers and mopups
   for (var i = 0; i < data.length; i++) {
     color = "red";
@@ -82,40 +116,27 @@ function displayData() {
         fillOpacity: 0.5,
         radius: 150,
       }
-    ).addTo(mymap);
+    ).addTo(leaflet_map);
+
     //adds popup to this circle
     circle.bindPopup(
-      "<h1>Hestagram</h1><img src='" +
-        data[i].postPhoto +
-        "' alt = 'images' class= 'herImg'><br>" +
+      "<h1>Hestagram</h1>" +
+        `<img src=${data[0].postPhoto} alt='images' class='herImg' /> <br>` +
         data[i].caption +
         "<br>" +
         data[i].date +
         "<br>" +
         data.length +
+        "<br>" +
         "<a href = 'gallery.html'>See more</a>"
     );
 
+    // var img = document.getElementById("img").src;
+    // img = data[0].postPhoto;
+
     //the pop up displays on hover intead of onClick as it is by default
-    circle.on("mouseover", function (ev) {
-      ev.target.openPopup();
+    circle.on("mouseover", function (event) {
+      event.target.openPopup();
     });
-  }
-}
-
-getSelectedOptionValue();
-//gets the value of choosesn location from the menu with options
-function getSelectedOptionValue() {
-  var selectedOption = document.getElementById("optionList").value;
-
-  newZoom = 13;
-  if (selectedOption === 1) {
-    //57.708870, 11.974560 gothenburg
-    console.log(selectedOption);
-    loadMap(51.505, 0, 13); //London
-  } else if (selectedOption === 2) {
-    loadMap(43.41667, 24.61667, newZoom); // Kaylaka park
-  } else if (selectedOption === 3) {
-    loadMap(56.87767, 14.80906, newZoom); // Vaxjo
   }
 }
